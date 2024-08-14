e32aa68e4f20d53efb3382c11773fbbd04b8b60c
Juan Jose Vera Briceño <juancho_2ce@hotmail.com>
2024-08-13 19:31:08 -0500

    Initial Commit

---
+++ Assets/Scripts/Player/Snail.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snail : MonoBehaviour
+{
+    private decimal distance = 0;
+
+    // m/s
+    private decimal speed = 1;
+    private decimal food = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
